0354f6b1b8229257a1b8d78ba35ba8c6d1938fd0
shikhavaidh <36124149+shikhavaidh@users.noreply.github.com>
2018-02-23 21:19:06 +0530

    Assignment 1.1
    Assignment 1.1

---
+++ Assignment 1.1.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# In[3]:
+
+print ("Hello World")
+
+
+# In[5]:
+
+N1 = 5
+N2 = 10
+#Add two integer numbers
+Sum = N1 + N2
+print (Sum)
+
+
+# In[6]:
+
+fN1 = 5.2
+fN2 = 10.5
+#Add two integer numbers
+fSum = fN1 + fN2
+print (fSum)
+
+
+# In[ ]:
+
+
+
